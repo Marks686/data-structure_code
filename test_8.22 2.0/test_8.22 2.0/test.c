e1509a2b061c256f11209d5890044eb3827c1b93
Marks686 <17829791810@163.com>
2022-08-22 23:13:23 +0800

    提交顺序表部分代码

---
+++ test_8.22 2.0/test_8.22 2.0/test.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+typedef int SLDataType;
+// 顺序表的动态存储
+typedef struct SeqList
+{
+	SLDataType* array;  // 指向动态开辟的数组
+	size_t size;       // 有效数据个数
+	size_t capicity;   // 容量空间的大小
+}SeqList;
+// 基本增删查改接口
+// 顺序表初始化
+
+void SeqListInit(SeqList* psl);
+// 检查空间，如果满了，进行增容
+void CheckCapacity(SeqList* psl);
+// 顺序表尾插
+void SeqListPushBack(SeqList* psl, SLDataType x);
+// 顺序表尾删
+void SeqListPopBack(SeqList* psl);
+// 顺序表头插
+void SeqListPushFront(SeqList* psl, SLDataType x);
+// 顺序表头删
+void SeqListPopFront(SeqList* psl);
+// 顺序表查找
+int SeqListFind(SeqList* psl, SLDataType x);
+// 顺序表在pos位置插入x
+void SeqListInsert(SeqList* psl, size_t pos, SLDataType x);
+// 顺序表删除pos位置的值
+void SeqListErase(SeqList* psl, size_t pos);
+// 顺序表销毁
+void SeqListDestory(SeqList* psl);
+// 顺序表打印
+void SeqListPrint(SeqList* psl);
+// 1、无头+单向+非循环链表增删查改实现
+typedef int SLTDateType;
+typedef struct SListNode
+{
+	SLTDateType data;
+	struct SListNode* next;
+}SListNode;
+// 动态申请一个结点
+SListNode* BuySListNode(SLTDateType x);
+// 单链表打印
+void SListPrint(SListNode* plist);
+// 单链表尾插
+void SListPushBack(SListNode** pplist, SLTDateType x);
+// 单链表的头插
+void SListPushFront(SListNode** pplist, SLTDateType x);
+// 单链表的尾删
+void SListPopBack(SListNode** pplist);
+// 单链表头删
+void SListPopFront(SListNode** pplist);
+// 单链表查找
+SListNode* SListFind(SListNode* plist, SLTDateType x);
+// 单链表在pos位置之后插入x
+// 分析思考为什么不在pos位置之前插入？
+
+void SListInsertAfter(SListNode* pos, SLTDateType x);
+// 单链表删除pos位置之后的值
+// 分析思考为什么不删除pos位置？
+void SListEraseAfter(SListNode* pos);
