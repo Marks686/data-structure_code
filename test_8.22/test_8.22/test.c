4db0733f8c0799a19f87503ffd146076d3019018
Marks686 <17829791810@163.com>
2022-08-22 23:09:24 +0800

    提交顺序表部分代码

---
+++ test_8.22/test_8.22/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+// 计算BubbleSort的空间复杂度？
+//void BubbleSort(int* a, int n)
+//{
+//	assert(a);
+//	for (size_t end = n; end > 0; --end)
+//	{
+//		int exchange = 0;
+//		for (size_t i = 1; i < end; ++i)
+//		{
+//			if (a[i - 1] > a[i])
+//			{
+//				Swap(&a[i - 1], &a[i]);
+//				exchange = 1;
+//			}
+//		}
+//		if (exchange == 0)
+//			break;
+//	}
+//}
+#include"SeqList.h"
+void TestSqList1()
+{
+	SL sl;
+	SLInt(sl);
+}
+
+void TestSqList2()
+{
+
+}
+
+int main()
+{
+	TestSeqList1();
+	return 0;
+}
+
