6ddf066a151c118b631ccb105eb2084fb65d96c3
Marks686 <17829791810@163.com>
2023-03-21 13:08:48 +0800

    leetcode27.

---
+++ 代码随想录/20230320-leetcode二分查找/20230320-leetcode27/test.cpp
@@ -0,0 +1,16 @@
+//27. ÒÆ³ýÔªËØ
+//https://leetcode.cn/problems/remove-element/
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.size(); fast++)
+        {
+            if (nums[fast] != val)
+            {
+                nums[slow++] = nums[fast];
+            }
+        }
+        return slow;
+    }
+};
